3d0d94ad0246782b427fe1da6737eb55e78171d9
KivancEge <kayakivancege@gmail.com>
2025-12-17 16:33:55 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,53 @@
+# İçeri Aktar
+from flask import Flask, render_template, request, send_from_directory
+
+
+app = Flask(__name__)
+
+# Form sonuçları 
+@app.route('/', methods=['GET','POST'])
+def index():
+    if request.method == 'POST':
+        # seçilen resmi almak
+        selected_image = request.form.get('image-selector')
+
+        # Görev #2. Metni almak
+        text_top = request.form['textTop']
+        text_bottom = request.form['textBottom']
+        
+
+        # Görev #3. Metnin konumunu almak
+        text_top_y = request.form['textTop_y']
+        text_bottom_y = request.form['textTop_y']
+       
+
+        # Görev #3. Metnin rengini almak
+        
+
+        return render_template('index.html', 
+                               # Seçilen resmi görüntüleme
+                               selected_image=selected_image, 
+
+                               # Görev #2. Metni görüntüleme
+                               text_top = text_top,
+                               text_bottom = text_bottom,
+                               
+
+                               # Görev #3. Rengi görüntüleme
+                               text_top_y = text_top_y,
+                               text_bottom_y = text_bottom_y
+                               
+                               
+                               # Görev #3. Metnin konumunu görüntüleme
+
+                               )
+    else:
+        # Varsayılan olarak ilk resmi görüntüleme
+        return render_template('index.html', selected_image='logo.svg')
+
+
+@app.route('/static/img/<path:path>')
+def serve_images(path):
+    return send_from_directory('static/img', path)
+
+app.run(debug=True)
